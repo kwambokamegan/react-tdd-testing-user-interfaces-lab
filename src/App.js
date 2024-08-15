45649d7c5f6fd84691d369f69f510523c1b230b1
Megan <megan.nyakina@student.moringaschool.com>
2024-08-15 17:47:27 +0300

    completes lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Megan</h1>
+      <img
+        src="https://media.istockphoto.com/id/1177615987/photo/happy-fashionable-model-on-white-background.webp?b=1&s=612x612&w=0&k=20&c=qGuQn2Dlo_1msDg4ET4UkFHpNh46r-XgZLIAnFYz0aE="
+        alt="Megan"
+        title="Megan"
+      />
+      <h2>About Me</h2>
+      <p>My biography</p>
+      <a href="https://github.com/kwambokamegan">Github</a>
+      <br />
+      <a href="https://www.linkedin.com/jobs/">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
